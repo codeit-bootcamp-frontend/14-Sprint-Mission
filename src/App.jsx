e8c3d28c2bf78d84e67bde28bfeb8c43bf39c6b2
callu9 <callu_9ine@naver.com>
2025-02-26 14:09:13 +0900

    create ItemDetail, BoardDetail page for temporary

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserContextProvider from "./contexts/UserContext";
 import AddItem from "./pages/additem/AddItem";
 import Boards from "./pages/boards/Boards";
+import BoardDetail from "./pages/boards/children/Boards";
 import Faq from "./pages/faq/Faq";
 import Home from "./pages/home/Home";
+import ItemDetail from "./pages/items/children/ItemDetail";
 import Items from "./pages/items/Items";
 import Login from "./pages/members/Login";
 import Signup from "./pages/members/Signup";
@@ -17,9 +19,15 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/items" element={<Items />} />
+          <Route path="/items">
+            <Route index element={<Items />} />
+            <Route path=":itemId" element={<ItemDetail />} />
+          </Route>
           <Route path="/additem" element={<AddItem />} />
-          <Route path="/boards" element={<Boards />} />
+          <Route path="/boards">
+            <Route index element={<Boards />} />
+            <Route path=":boardId" element={<BoardDetail />} />
+          </Route>
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/faq" element={<Faq />} />
         </Routes>
---
+++ src/pages/items/children/ItemDetail.jsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import HeaderNav from "../../components/HeaderNav";
+import { useUser } from "../../contexts/UserContext";
+
+export default function ItemDetail() {
+  const user = useUser();
+
+  return user ? (
+    <>
+      <HeaderNav />
+      <div>
+        <h1>임시 상품 상세 페이지</h1>
+      </div>
+    </>
+  ) : (
+    <Navigate to="/login" />
+  );
+}
