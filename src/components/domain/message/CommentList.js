9e4d76beefabd8d838d347fd8edf14bbf6833650
numi8462 <yhk8462@naver.com>
2025-03-25 22:22:01 +0900

    refactor: 문의하기 CommentList로 이동

--- src/components/domain/message/CommentList.js
+++ src/components/domain/message/CommentList.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
+import Button from '../../common/Button';
+import TextArea from '../../common/TextArea';
 import useComments from '../../hooks/useComments';
 
 function CommentList({ productId }) {
   const { comments, loading, error, refetch } = useComments(productId);
-  console.log('comments', comments);
+  const [newComment, setNewComment] = useState();
+
+  const handleAddCommentClick = async () => {
+    // const { comment } = await productService.createMessage(
+    //   newComment
+    // );
+    refetch();
+  };
 
   return (
-    <div>{!loading && comments.map((comment) => <div>{comment.id}</div>)}</div>
+    <div>
+      <div className="inquiryContainer">
+        <TextArea
+          label="문의하기"
+          type="text"
+          placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+        />
+        <div className="buttonContainer">
+          <Button onClick={() => handleAddCommentClick()}>등록</Button>
+        </div>
+      </div>
+      {!loading && comments.map((comment) => <div>{comment.id}</div>)}
+    </div>
   );
 }
 
--- src/pages/ProductItemPage.js
+++ src/pages/ProductItemPage.js
@@ -2,8 +2,6 @@ import { useParams } from 'react-router-dom';
 import './ProductItemPage.css';
 import Navbar from '../components/common/Navbar';
 import ProductItem from '../components/domain/ProductItem';
-import TextArea from '../components/common/TextArea';
-import Button from '../components/common/Button';
 import useProduct from '../components/hooks/useProduct';
 import CommentList from '../components/domain/message/CommentList';
 
@@ -11,24 +9,12 @@ function ProductItemPage() {
   const { id } = useParams();
   const { product, loading, error, refetch } = useProduct(id);
 
-  const handleCommentClick = () => {};
-
   return (
     <div>
       <Navbar isLoggedIn={true} />
       {!loading && (
         <main className="ProductPage">
           <ProductItem item={product} />
-          <div className="inquiryContainer">
-            <TextArea
-              label="문의하기"
-              type="text"
-              placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
-            />
-            <div className="buttonContainer">
-              <Button onClick={() => handleCommentClick()}>등록</Button>
-            </div>
-          </div>
           <CommentList productId={id} />
         </main>
       )}
