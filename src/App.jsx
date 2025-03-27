f99c3418d2ea040eb24f8c6d23fd56f3e41ce02a
ramongEE <rlatndus21049@gmail.com>
2025-03-27 22:30:17 +0900

    홈페이지 창 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
-import GlobalStyles from "./styles/GlobalStyles";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme'
+import GlobalStyles from './styles/GlobalStyles'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Items from './pages/Items/Items'
 
 function App() {
   return (
@@ -11,10 +13,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/items" element={<Items />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,175 @@
-import React from 'react';
+import React from 'react'
+import * as S from './Home.style'
+import Logo from '../../assets/image/Logo.png'
+import LogoFace from '../../assets/image/LogoFace.png'
+import HomeTop from '../../assets/image/home_top.png'
+import HomeBottom from '../../assets/image/home_bottom.png'
+import HomeHotItems from '../../assets/image/home_hot_items.png'
+import HomeSearch from '../../assets/image/home_search.png'
+import HomeRegister from '../../assets/image/home_register.png'
+import Facebook from '../../assets/svg/facebook.svg'
+import Instagram from '../../assets/svg/instagram.svg'
+import Twitter from '../../assets/svg/twitter.svg'
+import Youtube from '../../assets/svg/youtube.svg'
+import Button from '../../component/common/Button'
+import { useNavigate } from 'react-router-dom'
 
-const Home = () => {
+function Home() {
+  const navigate = useNavigate()
+
+  function goToPostId() {
+    navigate(`/login`)
+  }
+  function goToPostItems() {
+    navigate(`/items`)
+  }
   return (
-    <div>
-      <h1>Welcome to Home Page</h1>
-    </div>
-  );
+    <>
+      <S.HeaderTop>
+        <S.HeaderNav>
+          <S.HeaderLogo>
+            <a href="./" target="_self">
+              <S.HeaderLogoFace src={LogoFace} alt="판다마켓 로고 사진" />
+            </a>
+            <a href="./" target="_self">
+              <S.HeaderLogoName src={Logo} alt="판다마켓 로고 사진" />
+            </a>
+          </S.HeaderLogo>
+          <S.ButtonWrapper>
+            <Button
+              variant="primary"
+              size={48.5}
+              width={128}
+              onClick={goToPostId}
+            >
+              로그인
+            </Button>
+          </S.ButtonWrapper>
+        </S.HeaderNav>
+      </S.HeaderTop>
+      <S.HeaderMain>
+        <S.HeaderMainContainer>
+          <S.HeaderMainTitle>
+            <S.HeaderTitleFont>
+              일상의 모든 물건을 거래해 보세요
+            </S.HeaderTitleFont>
+
+            <Button
+              variant="primary"
+              size={56}
+              width={364}
+              onClick={goToPostItems}
+            >
+              구경하러 가기
+            </Button>
+          </S.HeaderMainTitle>
+          <img src={HomeTop} alt="판다마켓 백그라운드사진" />
+        </S.HeaderMainContainer>
+      </S.HeaderMain>
+      <S.MainBasic>
+        <S.MainTheme>
+          <S.MainPopularSellImage>
+            <img src={HomeHotItems} alt="판다마켓 인기 상품 사진" />
+          </S.MainPopularSellImage>
+          <S.MainThemeBasic>
+            <div>
+              <S.MainPopularSellFontTop>Hot item</S.MainPopularSellFontTop>
+            </div>
+            <div>
+              <S.MainPopularSellFontMiddle>
+                인기 상품을 확인해 보세요
+              </S.MainPopularSellFontMiddle>
+            </div>
+            <div>
+              <S.MainPopularSellFontBottom>
+                가장 HOT한 중고거래 물품을 판다 마켓에서 확인해 보세요
+              </S.MainPopularSellFontBottom>
+            </div>
+          </S.MainThemeBasic>
+        </S.MainTheme>
+        <S.MainThemeCenter>
+          <S.MainThemeBasicMiddle>
+            <div>
+              <S.MainPopularSellFontTop>Search</S.MainPopularSellFontTop>
+            </div>
+            <div>
+              <S.MainPopularSellFontMiddle>
+                구매를 원하는 상품을 검색하세요
+              </S.MainPopularSellFontMiddle>
+            </div>
+            <div>
+              <S.MainPopularSellFontBottom>
+                구매하고 싶은 물품은 검색해서&nbsp;쉽게 찾아보세요
+              </S.MainPopularSellFontBottom>
+            </div>
+          </S.MainThemeBasicMiddle>
+          <S.MainSearchImage>
+            <img src={HomeSearch} alt="판다마켓 상품 검색 사진" />
+          </S.MainSearchImage>
+        </S.MainThemeCenter>
+        <S.MainTheme>
+          <S.MainPopularSellImage>
+            <img src={HomeRegister} alt="판다마켓 인기 상품 등록" />
+          </S.MainPopularSellImage>
+          <S.MainThemeBasic>
+            <div>
+              <S.MainPopularSellFontTop>Register</S.MainPopularSellFontTop>
+            </div>
+            <div>
+              <S.MainPopularSellFontMiddle>
+                판매를 원하는 상품을 등록하세요
+              </S.MainPopularSellFontMiddle>
+            </div>
+            <div>
+              <S.MainPopularSellFontBottom>
+                어떤 물건이든 판매하고 싶은&nbsp; 상품을 쉽게 등록하세요
+              </S.MainPopularSellFontBottom>
+            </div>
+          </S.MainThemeBasic>
+        </S.MainTheme>
+      </S.MainBasic>
+      <S.FooterMainContainer>
+        <S.FooterEmpty></S.FooterEmpty>
+        <S.FooterContainer>
+          <S.FooterBackground>
+            <div>
+              <S.FooterFont>믿을 수 있는 판다마켓 중고 거래</S.FooterFont>
+            </div>
+            <S.FooterImage>
+              <img src={HomeBottom} alt="판다마켓 백그라운드사진" />
+            </S.FooterImage>
+          </S.FooterBackground>
+        </S.FooterContainer>
+        <S.FooterNav>
+          <S.FooterNavMain>
+            <S.Codeit>©codeit - 2024</S.Codeit>
+            <S.PrivacyFaq>
+              <S.Privacy href="./pages/privacy.html" target="_blank">
+                Privacy Policy
+              </S.Privacy>
+              <S.Faq href="./pages/faq.html" target="_blank">
+                FAQ
+              </S.Faq>
+            </S.PrivacyFaq>
+            <S.Social>
+              <a href="https://www.facebook.com/" target="_blank">
+                <img src={Facebook} alt="페이스북 로고 사진" />
+              </a>
+              <a href="https://x.com/" target="_blank">
+                <img src={Twitter} alt="트위터 로고 사진" />
+              </a>
+              <a href="https://www.youtube.com/" target="_blank">
+                <img src={Youtube} alt="유튜브 로고 사진" />
+              </a>
+              <a href="https://www.instagram.com/" target="_blank">
+                <img src={Instagram} alt="인스타그램 로고 사진" />
+              </a>
+            </S.Social>
+          </S.FooterNavMain>
+        </S.FooterNav>
+      </S.FooterMainContainer>
+    </>
+  )
 }
 
-export default Home;  
+export default Home
