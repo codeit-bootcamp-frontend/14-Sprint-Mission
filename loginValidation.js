296980aee724b59584f76f41258e0bf28e5d4809
Circle0616 <happy_faces@naver.com>
2025-03-29 18:46:04 +0900

    fix: 메세지 내용 수정

--- loginValidation.js
+++ loginValidation.js
@@ -5,15 +5,15 @@ const element = {
     input: document.getElementById("email"),
     error: document.getElementById("emailErrorMessage"),
     regex: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-    emptyMessage: "이메일을 입력해주세요",
+    emptyMessage: "이메일을 입력해주세요.",
     invalidMessage: "잘못된 이메일 형식입니다.",
   },
   password: {
     input: document.getElementById("password"),
     error: document.getElementById("passwordErrorMessage"),
     regex: /^.{8,}$/,
-    emptyMessage: "비밀번호를 입력해주세요",
-    invalidMessage: "비밀번호를 8자 이상 입력해주세요",
+    emptyMessage: "비밀번호를 입력해주세요.",
+    invalidMessage: "비밀번호를 8자 이상 입력해주세요.",
   },
 };
 
