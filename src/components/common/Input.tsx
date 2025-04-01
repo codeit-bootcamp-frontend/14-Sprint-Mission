463b41360c0a75ae224682545eeea24b65fcc953
numi8462 <yhk8462@naver.com>
2025-04-01 21:46:42 +0900

    feat: LoginPage 및 SignUpPage 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,10 +1,13 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, Ref, RefAttributes } from 'react';
 import './Input.css';
 
 interface InputProps {
   label: string;
   id: string;
   value: string;
+  type: string;
+  placeholder?: string;
+  ref?: Ref<HTMLInputElement>;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -12,7 +15,7 @@ function Input({ label, id, value, onChange, ...rest }: InputProps) {
   return (
     <div className="inputContainer">
       <label>{label}</label>
-      <input id={id} value={value} {...rest} />
+      <input id={id} value={value} onChange={onChange} {...rest} />
     </div>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,45 +1,88 @@
+import { ChangeEvent, useRef, useState } from 'react';
 import './LoginPage.css';
+import { Link } from 'react-router-dom';
+import LogoImg from '../assets/images/logo.png';
+import HidePasswordIcon from '../assets/icons/eye-slash.svg';
+import ShowPasswordIcon from '../assets/icons/eye.svg';
+import Input from '../components/common/Input';
+import GoogleLogo from '../assets/social/google.png';
+import KakaoLogo from '../assets/social/kakao.png';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 function LoginPage() {
+  const [inputData, setInputData] = useState<LoginData>({
+    email: '',
+    password: '',
+  });
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setInputData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
   return (
     <>
       <main>
-        <div className="container">
-          <a className="brand" href="/">
+        <div className="login-container">
+          <Link className="brand" to="/">
             <img
-              className="logo"
-              src="/images/logo.png"
+              className="login-logo"
+              src={LogoImg}
               alt="logo"
-              width="100"
-              height="100"
+              width={100}
+              height={100}
             />
             판다마켓
-          </a>
+          </Link>
           <form id="form">
             <div className="input-box">
-              <label htmlFor="email">이메일</label>
-              <input
+              <Input
+                label="이메일"
                 id="email"
-                name="email"
+                value={inputData.email}
                 type="email"
                 placeholder="이메일을 입력해주세요"
-                required
+                onChange={handleChange}
               />
             </div>
             <div className="input-box">
-              <label htmlFor="password">비밀번호</label>
-              <input
+              <Input
+                label="비밀번호"
                 id="password"
-                name="password"
-                type="password"
+                type={!showPassword ? 'password' : 'text'}
+                value={inputData.password}
                 placeholder="비밀번호를 입력해주세요"
-                required
+                onChange={handleChange}
+                ref={passwordInputRef}
               />
-              <img
-                id="eye-slash"
-                src="/images/eye-slash.svg"
-                alt="show password"
-              />
+              {!showPassword ? (
+                <img
+                  className="hide-password-icon"
+                  src={HidePasswordIcon}
+                  alt="hide password"
+                  onClick={togglePassword}
+                />
+              ) : (
+                <img
+                  className="show-password-icon"
+                  src={ShowPasswordIcon}
+                  alt="show password"
+                  onClick={togglePassword}
+                />
+              )}
             </div>
             <button type="button" className="login-btn" disabled>
               로그인
@@ -48,23 +91,15 @@ function LoginPage() {
           <div className="social-login">
             <span>간편 로그인하기</span>
             <a href="https://google.com">
-              <img
-                src="./images/social/google.png"
-                alt="google logo"
-                width="40"
-              />
+              <img src={GoogleLogo} alt="google logo" width="40" />
             </a>
             <a href="https://kakao.com">
-              <img
-                src="./images/social/kakao.png"
-                alt="google logo"
-                width="40"
-              />
+              <img src={KakaoLogo} alt="google logo" width="40" />
             </a>
           </div>
           <span>
             판다마켓이 처음이신가요?
-            <a href="/signup.html">회원가입</a>
+            <a href="/signup"> 회원가입</a>
           </span>
         </div>
       </main>
