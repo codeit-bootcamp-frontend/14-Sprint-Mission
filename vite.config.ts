226c4447d787c292ca23c8b9893eb52fd2c14fda
Circle0616 <happy_faces@naver.com>
2025-04-08 23:44:31 +0900

    :wrench: @를 이용한 절대경로 path alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
