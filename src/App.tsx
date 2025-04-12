652ee8329171e2a68be5e9787f6535af8ce08169
Circle0616 <happy_faces@naver.com>
2025-04-12 15:01:48 +0900

    :sparkles: 기본적인 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import Home from "@/pages/home.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<div>로그인 페이지</div>} />
+          <Route path="/signup" element={<div>회원가입 페이지</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
