00a6931787b283c74c5c253fdc63b4a8af082c31
Circle0616 <happy_faces@naver.com>
2025-04-19 23:52:59 +0900

    :sparkles: 입력 검증 로직 추가 및 오류 상태 스타일링 개선

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,9 @@
 import { createContext, useContext, useState } from "react";
 import styles from "./input.module.css";
+import inputValidate from "@/utils/input-validate";
 
-type inputType = "email" | "username" | "password";
+// type, interface 선언
+export type inputType = "email" | "username" | "password";
 
 interface InputProps {
   type: inputType;
@@ -18,10 +20,13 @@ interface InputFieldProps {
 
 interface InputContextProps {
   value: string;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type: inputType;
+  error: boolean;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
+//input 컴포넌트 들에서 사용할 컨텍스트 생성, 커스텀 훅 생성
 const InputContext = createContext<InputContextProps | undefined>(undefined);
 
 const useInputContext = (): InputContextProps => {
@@ -32,20 +37,33 @@ const useInputContext = (): InputContextProps => {
   return context;
 };
 
+//Input요소들을 감싸는 컨테이너 역할을 할 Input.Group컴포넌트 작성
+//상태관리, 이벤트 리스너 생성해서 내려줍니다.
 function InputGroup({ type, children }: InputProps) {
   const [value, setValue] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const isValid = inputValidate(type, value);
+    console.log(isValid);
+    setError(!isValid);
+  };
+
   return (
-    <InputContext.Provider value={{ value, handleChange, type }}>
+    <InputContext.Provider
+      value={{ value, type, error, handleChange, handleBlur }}
+    >
       <div className={styles["input__group"]}>{children}</div>
     </InputContext.Provider>
   );
 }
 
+//단순히 라벨 렌더링만 하는 Input.Label 컴포넌트 입니다.
 function InputLabel({ label }: InputLabelProps) {
   return (
     <label className={`${styles["input__label"]} font-2lg font-bold`}>
@@ -55,17 +73,20 @@ function InputLabel({ label }: InputLabelProps) {
 }
 
 function InputField({ placeholder }: InputFieldProps) {
-  const { value, handleChange, type } = useInputContext();
+  const { value, type, error, handleChange, handleBlur } = useInputContext();
 
   const inputType = type === "username" ? "text" : type;
 
   return (
     <input
-      className={styles["input__field"]}
+      className={`${styles["input__field"]} ${
+        error ? styles["input__field--error"] : ""
+      }`}
       type={inputType}
       onChange={handleChange}
       placeholder={placeholder}
       value={value}
+      onBlur={handleBlur}
     />
   );
 }
