b45405c4353c994b757b795af51d9aa951da66fc
Circle0616 <happy_faces@naver.com>
2025-04-19 23:52:19 +0900

    :sparkles: 페이지네이션 컴포넌트 및 스타일 추가

---
+++ src/components/pagination.tsx
@@ -0,0 +1,43 @@
+import { SetStateAction } from "react";
+import styles from "./pagination.module.css";
+
+export interface PaginationProps {
+  currentPage: number;
+  totalPage: number;
+  setPage: React.Dispatch<SetStateAction<number>>;
+}
+
+export default function Pagination({
+  currentPage,
+  totalPage,
+  setPage,
+}: PaginationProps) {
+  let pages;
+  if (currentPage <= 3) {
+    pages = Array.from({ length: 5 }, (_, i) => i + 1);
+  } else if (currentPage >= totalPage - 3) {
+    pages = Array.from({ length: 5 }, (_, i) => i + totalPage - 4);
+  } else {
+    pages = Array.from({ length: 5 }, (_, i) => i + currentPage - 2);
+  }
+
+  const handleClick = (page: number) => {
+    setPage(page);
+  };
+  return (
+    <ul className={styles.pagesList}>
+      {pages.map((page) => (
+        <li key={page - 1} className={styles.el}>
+          <button
+            className={`${styles.pageBtn} ${
+              page === currentPage && styles.active
+            }`}
+            onClick={() => handleClick(page)}
+          >
+            {page}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
