b78ebf635cd8a76a24a5dc57e23baa05b791342c
Circle0616 <happy_faces@naver.com>
2025-04-19 23:53:19 +0900

    :sparkles: 아이템 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "@/pages/home/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/login";
 import SignUp from "./pages/sign-up/sign-up";
+import Items from "./pages/items/items";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/items" element={<Items />} />
         </Routes>
       </BrowserRouter>
     </>
